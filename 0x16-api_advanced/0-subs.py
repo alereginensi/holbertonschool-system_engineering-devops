9023ffb03eaae6b9a7bc3acb24d2ed1c1c70704c
alereginensi <reginensia@gmail.com>
2022-09-21 12:08:26 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 '''queries the Reddit API and returns the number of subscribers for a given subreddit.'''
 '''If an invalid subreddit is given, the function should return 0.'''
 
-import requests
 import sys
 
 def number_of_subscribers(subreddit):
