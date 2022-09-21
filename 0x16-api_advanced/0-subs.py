d9a224cee156089dd18546912fe7fc01a826a789
alereginensi <reginensia@gmail.com>
2022-09-21 12:35:34 -0700

    try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''queries the Reddit API and returns the number of subscribers for a given subreddit.'''
 '''If an invalid subreddit is given, the function should return 0.'''
-
 import requests
 
 
@@ -9,14 +8,10 @@ def number_of_subscribers(subreddit):
     '''queries the Reddit API'''
     try:
         url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = requests.utils.default_headers()
         headers = {
             'User-Agent': 'My User Agent 1.0'
         }
-
-        response = requests.get(url, headers=headers).json()
-        subs = response['data']['subscribers']
-
-        return subs
-    except Exception:
+        response = requests.get(url, headers=headers)
+        return response.json()['data']['subscribers']
+    except Exception as e:
         return 0
