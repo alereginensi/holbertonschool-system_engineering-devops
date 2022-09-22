245318056203d732bd28d912eda6f018ad98a556
alereginensi <reginensia@gmail.com>
2022-09-22 11:45:50 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-exec {'fix_500_error':
+# Using strace, find out why Apache is returning a 500 error.
+
+exec {'fixing':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
